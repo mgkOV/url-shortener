0d4dd75e112e6c0040a33a5e26e6856631dace1e
Oleg M <myagkov_85@mail.ru>
2016-11-23 15:18:15 +0300

    finish backend

--- helpers/index.js
+++ helpers/index.js
@@ -2,29 +2,50 @@ const moment = require('moment');
 const Link = require('../db').Link;
 const isURL = require('validator').isURL;
 
+// Save link in db
+var saveLink = (req, res, next) => {
+  var link = new Link({
+    url: req.orgURL,
+    shortLink: req.shortLink,
+    date: moment().format("MMMM DD YYYY, HH:mm (Z)")
+  });
+
+  link.save()
+    .catch((error) => {
+      console.log(error);
+    });
+  next();
+};
+
+//Chek if short link exist in db
 var findShortLink = (req, res, next) => {
   Link.findOne({'shortLink': req.params.link})
     .then((link) => {
       if (link) {
-        res.redirect(`http://${link.url}`)
+        res.redirect(link.url);
       } else {
         next();
       }
     });
 }
 
+// Validate if link correct formated
 var validateLink = (req, res, next) => {
-  var valid = isURL(req.params.link);
+  var valid = isURL(req.originalUrl.slice(1), {
+    require_protocol: true
+  });
   if (valid) {
+    req.orgURL = req.originalUrl.slice(1);
     next();
   } else {
-    res.send('You provide bad-formated URL as a parameter');
+    res.send('You provided bad-formated URL as a parameter');
   }
 };
 
+//Create short link alias
 var shortenLink = (req, res, next) => {
   //Check if URL exists in db
-  Link.findOne({'url': req.params.link})
+  Link.findOne({'url': req.orgURL})
     .then((link) => {
       if (link) {
         req.shortLink = link.shortLink;
@@ -37,27 +58,16 @@ var shortenLink = (req, res, next) => {
             if (link) {
               shortenLink(req, res, next)
             } else {
-              next();
+              saveLink(req, res, next);
             }
           });
       }
     });
 };
 
-var saveLink = (req, res, next) => {
-  var link = new Link({
-    url: req.params.link,
-    shortLink: req.shortLink,
-    date: moment().format("MMMM DD YYYY, HH:mm (Z)")
-  });
-
-  link.save()
-  next();
-};
 
 module.exports = {
   findShortLink,
   validateLink,
-  shortenLink,
-  saveLink
+  shortenLink
 };
--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const h = require('./helpers');
 const port = process.env.PORT || 3000
 
 app
-  .use('/:link', h.findShortLink
+  .use('/:link*', h.findShortLink
                , h.validateLink
-               , h.shortenLink
-               , h.saveLink)
+               , h.shortenLink)
   .get('/', (req, res) => {
     res.send('Home page');
   })
-  .get('/:link', (req, res) => {
-    res.send(req.shortLink);
+  .get('/:link*', (req, res) => {
+    var linkObj = {
+      original_url: req.orgURL,
+      short_url: req.shortLink
+    }
+    res.json(linkObj);
   }).listen(port, () => console.log('App running on port', port));
