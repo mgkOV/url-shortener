a2c51cf4742b18bd55baad6b7cb0b55822db12e0
mww <myagkov.mww@gmail.com>
2016-11-23 22:40:59 +0300

    add view engine

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const h = require('./helpers');
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app
   .use('/:link*', h.findShortLink
                , h.validateLink
                , h.shortenLink)
   .get('/', (req, res) => {
-    res.send('Home page');
+    res.render('index');
   })
   .get('/:link*', (req, res) => {
     var linkObj = {
